53fbc391560577aeecd34b60702a178da1c4779e
Kishor Rathva <kishor.rathva@rapidops.com>
2019-03-20 19:06:32 +0530

    meanBack

---
+++ src/app/shared/country.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
+import { Country } from '../country'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CountryService {
+
+  private country:Country;
+  private baseURI : string = 'http://localhost:8080';
+  private headers = new HttpHeaders().set('Content-Type','application/json')
+  constructor(private http:HttpClient) { }
+
+  createCountry(country:Country){
+
+    return this.http.post(this.baseURI + '/', country,{ headers:this.headers});
+  }
+  readCountries(){
+
+    return this.http.get(this.baseURI + '/',{ headers:this.headers});
+  }
+
+  updateCountry(country:Country) {
+
+    return this.http.put(this.baseURI+ '/', country, { headers:this.headers});
+  }
+  deleteCountry(id:string){
+
+    return this.http.delete(this.baseURI+'/'+id,{ headers:this.headers});
+  }
+
+  setter(country:Country){
+    this.country=country;
+  }
+
+  getter(){
+    return this.country;
+  }
+}
