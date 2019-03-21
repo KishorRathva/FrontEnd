e56a30f45a209fe374ad818b9d60fead32aba146
kishor <kishorrathva8298@gmail.com>
2019-03-21 13:34:43 +0530

    Changed path

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,46 +1,48 @@
-import { Country } from './../../country';
-import { Component, OnInit } from '@angular/core';
-import { CountryService } from '../../shared/country.service';
-import { Country } from '../../country';
-import { Router } from '@angular/router';
+import { Country } from "./../../country";
+import { Component, OnInit } from "@angular/core";
+import { CountryService } from "../../shared/country.service";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-list',
-  templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+  selector: "app-list",
+  templateUrl: "./list.component.html",
+  styleUrls: ["./list.component.css"]
 })
 export class ListComponent implements OnInit {
-  private countries:Country[];
+  private countries: Country[];
 
-  constructor(private _countryService:CountryService , private router:Router) { }
+  constructor(
+    private _countryService: CountryService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
     this.readCountries();
   }
-  readCountries(){
+  readCountries() {
     this._countryService.readCountries().subscribe(
       data => {
         console.log(data);
-        this.countries= data['msg'];
-        console.log(this.countries)
+        this.countries = data["msg"];
+        console.log(this.countries);
       },
       error => {
         console.log(error);
       }
-    )
+    );
   }
 
-  doUpdate(country){
+  doUpdate(country) {
     this._countryService.setter(country);
-    this.router.navigate(['/createUpdate']);
+    this.router.navigate(["/createUpdate"]);
   }
 
-  doDelete(country){
-    this._countryService.deleteCountry(country._id).subscribe(data => {
-      this.countries.splice(this.countries.indexOf(Country),1);
-    },error => {
-
-    })
+  doDelete(country) {
+    this._countryService.deleteCountry(country._id).subscribe(
+      data => {
+        this.countries.splice(this.countries.indexOf(country), 1);
+      },
+      error => {}
+    );
   }
-
 }
--- src/app/shared/country.service.ts
+++ src/app/shared/country.service.ts
@@ -1,40 +1,39 @@
-import { Injectable } from '@angular/core';
-import { HttpClient,HttpHeaders } from '@angular/common/http';
-import { Country } from '../country'
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Country } from "../country";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class CountryService {
+  private country: Country;
+  private baseURI: string = "http://localhost:8080/";
+  private headers = new HttpHeaders().set("Content-Type", "application/json");
+  constructor(private http: HttpClient) {}
 
-  private country:Country;
-  private baseURI : string = 'http://localhost:8080';
-  private headers = new HttpHeaders().set('Content-Type','application/json')
-  constructor(private http:HttpClient) { }
-
-  createCountry(country:Country){
-
-    return this.http.post(this.baseURI + '/', country,{ headers:this.headers});
+  createCountry(country: Country) {
+    return this.http.post(this.baseURI, country, {
+      headers: this.headers
+    });
   }
-  readCountries(){
-
-    return this.http.get(this.baseURI + '/',{ headers:this.headers});
-  }
-
-  updateCountry(country:Country) {
-
-    return this.http.put(this.baseURI+ '/', country, { headers:this.headers});
-  }
-  deleteCountry(id:string){
-
-    return this.http.delete(this.baseURI+'/'+id,{ headers:this.headers});
+  readCountries() {
+    return this.http.get(this.baseURI, { headers: this.headers });
   }
 
-  setter(country:Country){
-    this.country=country;
+  updateCountry(country: Country) {
+    return this.http.put(this.baseURI, country, {
+      headers: this.headers
+    });
+  }
+  deleteCountry(id: string) {
+    return this.http.delete(this.baseURI + id, { headers: this.headers });
   }
 
-  getter(){
+  setter(country: Country) {
+    this.country = country;
+  }
+
+  getter() {
     return this.country;
   }
 }
